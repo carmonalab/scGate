d009298da0d8e3edab4829a810c8837e95589afa
Massimo Andreatta <massimo.andreatta@unil.ch>
2021-02-16 18:49:17 +0100

    Update man pages

--- R/main.R
+++ R/main.R
@@ -3,9 +3,9 @@
 #' Apply CTfilter for a specific cell type to a query dataset
 #'
 #' @param query Seurat object containing a query data set - filtering will be applied to this object
-#' @param celltype Cell type to preserve from the query data set (should be one of cell types in `names(markers)`)
-#' @param CT.thresholds A named list with thresholds for each cell type - see function `calculate_thresholds_CTfilter`
-#' @param markers List of markers for each cell type, for example `CTfilter::MCA.markers.Mm`
+#' @param celltype Cell type to preserve from the query data set (should be one of cell types in \code{names(markers)})
+#' @param CT.thresholds A named list with thresholds for each cell type - see function \code{\link{calculate_thresholds_CTfilter}}
+#' @param markers List of markers for each cell type, for example \code{CTfilter::MCA.markers.Mm}
 #' @param max.impurity Maximum fraction of impurity allowed in clusters to flag cells as "pure"
 #' @param ndim Number of dimensions for cluster analysis
 #' @param resul Resolution for cluster analysis
@@ -16,13 +16,13 @@
 #' @param skip.normalize Skip data normalization
 #' @param seed Random seed for cluster analysis
 #' @param verbose Verbose output
-#' @return A new metadata column `is.pure` is added to the query Seurat object, indicating which cells correspond to the desidered cell type.
-#'     The `active.ident` is also set to this variable. Additionally, scores for all signatures in `markers` are added to the metadata of the Seurat object.
+#' @return A new metadata column \code{is.pure} is added to the query Seurat object, indicating which cells correspond to the desidered cell type.
+#'     The \code{active.ident} is also set to this variable. Additionally, scores for all signatures in \code{markers} are added to the metadata of the Seurat object.
 #' @examples
 #' query <- CTfilter(query, celltype="T.cell")
 #' DimPlot(query)
 #' query <- subset(query, subset=is.pure=="Pure")
-#' @seealso [calculate_thresholds_CTfilter()] to calculate celltype-specific thresholds
+#' @seealso \code{\link{calculate_thresholds_CTfilter()}} to calculate celltype-specific thresholds
 #' @export
 CTfilter <- function(query, celltype="T.cell", CT.thresholds=NULL, markers=NULL, max.impurity=0.5,
                      ndim=5, resol=3, assay="RNA", genes.blacklist=NULL, min.gene.frac=0.5, rm.existing=TRUE,
@@ -109,21 +109,21 @@ CTfilter <- function(query, celltype="T.cell", CT.thresholds=NULL, markers=NULL,
 #' Given a reference set, calculate thresholds that identify outliers from the reference
 #'
 #' @param ref Seurat object containing the reference data set
-#' @param markers List of markers for each cell type, for example `CTfilter::MCA.markers.Mm`
+#' @param markers List of markers for each cell type, for example \code{CTfilter::MCA.markers.Mm}
 #' @param sd.dev Maximum standard deviations from mean to identify outliers
 #' @param quant Quantile cutoff for score distribution
 #' @param assay Seurat assay to use
 #' @param min.gene.frac Only consider signatures covered by this fraction of genes in query set
 #' @param rm.existing Overwrite existing CTfilter scores in query object
 #' @param verbose Verbose output
-#' @return A new metadata column `is.pure` is added to the query Seurat object, indicating which cells correspond to the desidered cell type.
-#'     The `active.ident` is also set to this variable. Additionally, scores for all signatures in `markers` are added to the metadata of the Seurat object.
+#' @return Return the \code{ref} reference object, with celltype-specific thresholds in the field \code{ref@@misc$CTfilter}. Scores for individual signatures are
+#'     returned as metadata in the Seurat object
 #' @examples
 #' library(ProjecTILs)
 #' ref <- load.reference.map()
 #' ref <- calculate_thresholds_CTfilter(ref)
 #' ref@@misc$CTfilter
-#' @seealso [CTfilter()] to apply signatures on a query dataset and filter on a specific cell type
+#' @seealso \code{\link{CTfilter()}} to apply signatures on a query dataset and filter on a specific cell type
 #' @export
 calculate_thresholds_CTfilter <- function(ref, markers=NULL, sd.dev=7, quant=0.995, assay="RNA", min.gene.frac=0.5,
                                           rm.existing=TRUE, verbose=TRUE) {
