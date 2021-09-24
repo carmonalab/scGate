98c4b57f0000cd7bee5ce13c6dfc13adb705c79c
ajberenstein <arieljberenstein@gmail.com>
2021-09-24 12:38:48 -0300

    add signature's imputation function

--- R/utils.R
+++ R/utils.R
@@ -174,3 +174,44 @@ table.to.model <- function(scGate.table){
   }
   return(mod)
 }
+
+## This function allows to complete signatures in a table based model by using the name signature and a provided master.table of signatures
+# the master.table must be a two column data.frame with two columns : 1) name: contains the signature names and 
+# 2)signature: this column contain the genes present in each signature (separated with a semicolon) 
+require("dplyr")
+impute_signatures_from_name <- function(df.model,master.table ,name = "name",descript = "signature"){
+  
+  ## First, check if descript field exists in input table
+  if(!descript %in% colnames(df.model)){
+    df.model[descript] <- ""
+  }
+  
+  ## second, check if there is something to do (or exit)
+  input.sign <- df.model[[descript]]
+  input.names <- df.model[[name]]
+  complete.from.master.table <- as.vector(is.null(input.sign)|input.sign == "" | is.na(input.sign))
+  
+  if(!any(complete.from.master.table)){
+    message("nothing to do, the model signatures are already provided")
+    return(df.model)
+  }
+  # sanity check:
+  warn <- setdiff(input.names[complete.from.master.table],master.table[[name]])
+  if(length(warn)>0){
+    stop(sprintf("signatures '%s' are not present in the provided master.signature table",paste(warn,collapse = " ; " )))
+  }
+  
+  merged <- merge(df.model[complete.from.master.table,],master.table,by = name,suffixes = c("","_from.master"),all.x = T)
+  vect <- merged[[paste0(descript,"_from.master")]]
+  names(vect) <- merged[[name]]
+  if(merged%>%nrow > sum(complete.from.master.table)){
+    stop("please, master.table must not contain duplicated signature names")
+  }
+  
+  # replace ensuring correct order (notice that the merge can change row order)
+  nms <- df.model[complete.from.master.table,name]
+  df.model[complete.from.master.table,descript] <- vect[nms]
+  #  df.model[descript] <- output.sign
+  return(df.model)
+}
+
